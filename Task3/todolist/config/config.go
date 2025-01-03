dc7b8b028deb8cc558e7a0ae9c8ba134c887f02e
eeezds <2425863684@qq.com>
2025-01-03 19:25:11 +0800

    config1.0

--- Task3/todolist/config/config.go
+++ Task3/todolist/config/config.go
@@ -1,57 +1,66 @@
 package config
 
 import (
-    "fmt"
-    "gopkg.in/ini.v1"
+	"fmt"
+	"log"
+
+	"github.com/spf13/viper"
 )
 
 type DBData struct {
-    Dbhost     string
-    Dbport     string
-    Dbuser     string
-    Dbpassword string
-    Dbdatabase string
+	Dbhost     string
+	Dbport     string
+	Dbuser     string
+	Dbpassword string
+	Dbdatabase string
 }
 
 type ServiceData struct {
-    AppMode  string
-    HttpPort string
+	AppMode  string
+	HttpPort string
 }
 
 type Config struct {
-    DB      DBData
-    Service ServiceData
+	DB      DBData
+	Service ServiceData
 }
 
 func LoadConfig() (*Config, error) {
-    file, err := ini.Load("./config/config.yml")
-    if err != nil {
-        return nil, err
-    }
+	viper.SetConfigName("config.yml")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./")
+	err := viper.ReadInConfig() //读取配置
+	if err != nil {
+		log.Fatalln(err)
+		return nil, err
+	}
 
-    config := &Config{}
-    err = file.Section("mysql").MapTo(&config.DB)
-    if err != nil {
-        return nil, err
-    }
+	config := &Config{}
+	configDB := viper.Sub("mysql")
+	err = configDB.Unmarshal(&config.DB)
+	if err != nil {
+		log.Fatalln(err)
+		return nil, err
+	}
+	configService := viper.Sub("service")
+	err = configService.Unmarshal(&config.Service)
+	if err != nil {
+		log.Fatalln(err)
+		return nil, err
+	}
 
-    err = file.Section("service").MapTo(&config.Service)
-    if err != nil {
-        return nil, err
-    }
-
-    return config, nil
+	return config, nil
 }
 
 func (c *Config) DSN() string {
-    return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-        c.DB.Dbuser, c.DB.Dbpassword, c.DB.Dbhost, c.DB.Dbport, c.DB.Dbdatabase)
+	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		c.DB.Dbuser, c.DB.Dbpassword, c.DB.Dbhost, c.DB.Dbport, c.DB.Dbdatabase)
 }
 
 func Init() (*Config, error) {
-    config, err := LoadConfig()
-    if err != nil {
-        return nil, err
-    }
-    return config, nil
+	config, err := LoadConfig()
+	if err != nil {
+		return nil, err
+	}
+	return config, nil
 }
--- Task3/todolist/task/init.go
+++ Task3/todolist/task/init.go
@@ -1,17 +1,31 @@
 package task
 
 import (
-    "gorm.io/driver/mysql"
-    "gorm.io/gorm"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 var db *gorm.DB
 
 func Init(dsn string) (*gorm.DB, error) {
-    var err error
-    db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-    if err != nil {
-        return nil, err
-    }
-    return db, nil
+	var err error
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	// 获取底层的 *sql.DB 对象
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	// 设置连接池参数
+	sqlDB.SetMaxOpenConns(100)          // 设置最大打开连接数
+	sqlDB.SetMaxIdleConns(50)           // 设置最大空闲连接数
+	sqlDB.SetConnMaxLifetime(time.Hour) // 设置连接的最大生存时间
+
+	return db, nil
 }
